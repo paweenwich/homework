daee1f032e52c6250435437ad1ba5b4b0dea27c4
paweenwich <paweenwich@yahoo.com>
2021-09-23 21:04:31 +0700

    xxx

--- FindMaxKth/makedata.cpp
+++ FindMaxKth/makedata.cpp
@@ -7,7 +7,7 @@ using namespace std;
 void makeAnswer(vector<int> &data,int k)
 {
     //sort(data.begin(),data.end());
-    //cout << data[data.size() - k];
+    //cout << data[data.size() - k] << "\n"; 
 
     priority_queue<int, vector<int>,greater<int>> pq;
     for(int i=0;i<k;i++){
