a5f216161f39256e7de720bccf463434fd3ca431
paweenwich <paweenwich@yahoo.com>
2021-09-23 13:23:38 +0700

    FindMaxKth

---
+++ FindMaxKth/README.md
@@ -0,0 +1,21 @@
+# Find k’th largest element in an array
+https://www.techiedelight.com/find-kth-largest-element-array/
+
+Given an integer array, find k’th largest element in the array where k is a positive integer less than or equal to the length of array.
+
+## ข้อมูลเข้า
+บรรทัดแรกประกอบด้วยจำนวนเต็ม 2 ตัวคือ m เป็นความยาวของ ข้อมูล และ k เป็น ตำแหน่งที่ มากที่สุดที่ ต้องการหา 
+หลังจากป็นข้อมูล a จำนวน m ตัว  โดย  a <= 1000000
+  
+## ข้อมูลส่งออก
+ค่าของข้อมูลตำแหน่งที่มากที่สุด kth
+## Example
+### Input
+~~~
+6 2
+7 4 6 3 9 1
+~~~
+### Output
+~~~
+7
+~~~
---
+++ FindMaxKth/makedata.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <random>
+using namespace std;
+
+void makeAnswer(vector<int> &data,int k)
+{
+    //sort(data.begin(),data.end());
+    //cout << data[data.size() - k];
+
+    priority_queue<int, vector<int>,greater<int>> pq;
+    for(int i=0;i<k;i++){
+        pq.push(data[i]);
+    }
+    for(int i=k;i<data.size();i++){
+        if(pq.top() < data[i]){
+            pq.pop();
+            pq.push(data[i]);
+        }
+    }
+    cout << pq.top() << "\n";
+}
+
+
+
+
+int main(int argc,char * argv[])
+{
+    //std::random_device rd;
+    //std::time_t result = std::time(nullptr);
+    std::default_random_engine generator(0);
+    if(argc != 4){
+        printf("USAGE %s m k r\n",argv[0]);
+        return -1;
+    }
+
+    int m,k,r;
+    m = atoi(argv[1]);
+    k = atoi(argv[2]);
+    r = atoi(argv[3]);
+    cout << m << endl;
+    vector<int> data;
+    std::uniform_int_distribution<long long unsigned> dpoint(0,r);  // inclusive random
+    for(int i=0;i< m;i++){
+        int index = dpoint(generator);
+        cout << index << " ";
+        data.push_back(index);
+    }
+    cout << endl;
+    cout << "#output" << endl;
+    makeAnswer(data,k);
+}
